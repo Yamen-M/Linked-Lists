9e8f1b1d37563bc86469b8f5d8a43132778e4607
Yamen Morcel <77121917+Yamen-M@users.noreply.github.com>
2025-07-27 16:18:17 +0300

    more testing code

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
 import LinkedList from './linkedlist.js'
 
-// const list = new LinkedList();
-// list.append(5);
-// list.append(10);
-// list.prepend(2);
-// console.log(list.head);  // Should show node with value 2
-// console.log(list.tail);  // Should show node with value 10
+// example uses class syntax - adjust as necessary
+const odinList = new LinkedList();
 
-// console.log(list.head.value);        // 2
-// console.log(list.head.nextNode.value); // 5
-// console.log(list.tail.value);       // 10
-// console.log(list.tail.nextNode);    // null
-// console.log(list.getTail()); // 2
-// console.log(list.size()) //3
-// list.pop()
-// console.log(list.tail.value);       // 5 noice!
-// console.log(list.toString());
-// list.append(10);
-// console.log(list.toString());
+odinList.append("dog");
+odinList.append("cat");
+odinList.append("parrot");
+odinList.append("hamster");
+odinList.append("snake");
+odinList.append("turtle");
+
+console.log(odinList.toString());
 
 const list = new LinkedList();
 
 // === Initial State ===
-console.log("Initial list:", list.toString()); // null
+console.log("\n\nInitial list:", list.toString()); // null
 
 // === Append values ===
 list.append(10);
